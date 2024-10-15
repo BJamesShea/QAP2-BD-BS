1d06b09fd5fcebbc995c2f503061444a5279b630
Brandon Shea <brandon.shea1994@icloud.com>
2024-10-15 17:32:01 -0230

    students insert

--- problem1.sql
+++ problem1.sql
@@ -19,7 +19,6 @@ CREATE TABLE courses (
     course_description VARCHAR(300),
     professer_id INT,
     FOREIGN KEY (professor_id) REFERENCES professors(id)
-
 );
 
 CREATE TABLE enrollments(
@@ -30,3 +29,16 @@ CREATE TABLE enrollments(
     FOREIGN KEY (student_id) REFERENCES  students(id),
     FOREIGN KEY (course_id) REFERENCES courses(id)
 );
+
+INSERT INTO students (first_name, last_name, email, enrollment_date) VALUES
+('Nell', 'Crain', 'nellcrain@thohh.com', '1999-01-02'), --Haunting of Hill House the greatest TV show ever made
+('Dani', 'Clayton', 'daniclayton@thobm.com', '1928-11-4'), --Haunting of Bly Manor the second greatest TV show ever made
+('Paul', 'Hill', 'paulhill@midnightmass.com', '1991-02-17'), --Midnight Mass the third greatest TV show ever made
+('Robert', 'Baratheon', 'robbaratheon@got.com', '1674-07-20'),
+('Barney', 'Stinson', 'barnetstinson@himym.com', '1987-10-11');
+
+INSERT INTO professors (first_name, last_name, department) VALUES
+('Barack', 'Obama', 'Presidential debate class'),
+('Justin', 'Trudeau', 'Hockey class'),
+('Albert', 'Einstein', 'Basic Math'),
+('John', 'Mayer', 'Advanced Music');
