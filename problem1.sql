b1969b2498a5317c92027d1a89c02199b225e003
Brandon Shea <brandon.shea1994@icloud.com>
2024-10-15 16:58:58 -0230

    Auto increment as per instructions, id serial

--- problem1.sql
+++ problem1.sql
@@ -1,7 +1,11 @@
 CREATE TABLE students (
-    id SERIAL PRIMARY KEY,
+    id INT AUTO_INCREMENT PRIMARY KEY,
      first_name VARCHAR(50),
      last_name VARCHAR(50),
      email VARCHAR(100),
      school_enrollment_date DATE
 );
+
+CREATE TABLE professors (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+)
